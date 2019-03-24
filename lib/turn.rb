ae467a86c6b3e2ac324a0a3f39b7971dd3b2ca9a
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-24 17:56:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,11 +16,19 @@ def move(board, index, token = "X")
 end
 
 # code your #valid_move? method here
+#def valid_move?(board, index)
+#  if !index.between?(0, 8)
+#    false
+#  elsif !position_taken?(board, index)
+#    true
+#  end
+#end
+
 def valid_move?(board, index)
-  if !index.between?(0, 8)
-    false
-  elsif !position_taken?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
     true
+  else
+    false
   end
 end
 
